8c49dfa32908e647aebfc87c579a1be065d240c2
DreamerAEKer <dreameraeker@gmail.com>
2026-06-09 11:43:08 +0700

    Configure for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { Package, User } from 'lucide-react';
 import CustomerForm from './pages/CustomerForm';
 import StaffPortal from './pages/StaffPortal';
